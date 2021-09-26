93137d256d5f4c6e16364621b41d99d4407cbc71
huzhihui <huzhihui.com>
2021-09-26 17:24:28 +0800

    完成固定格式涉疫中高风险地区漫入信息excel分解

--- police-node/serve.js
+++ police-node/serve.js
@@ -40,8 +40,10 @@ var getMonth = (index, res) => {
       // var contentName = ''
       if (err) throw err
       console.log(data)
+      var highRisk = /[\u5168\u56fd\u6d89\u75ab]{1}[\u4e00-\u9fa5]+[\u6f2b\u5165\u6c5f\u897f]{1}[\u4e00-\u9fa5]+[0-9]*/
         for (const key of data) {
-          if (key === '涉疫重点关注手机号码.xlsx' || key === '涉疫重点关注手机号码.xls') {
+          // if (key === '涉疫重点关注手机号码.xlsx' || key === '涉疫重点关注手机号码.xls') {
+          if (highRisk.test(key)) {
             var excelName = ''
             var sheetName = ''
             excelName = key
@@ -60,6 +62,7 @@ var getMonth = (index, res) => {
                   var userTableData0 = [];var userTableData1 = [];var userTableData2 = [];var userTableData3 = [];var userTableData4 = [];
                   var userTableData5 = [];var userTableData6 = [];var userTableData7 = [];var userTableData8 = [];var userTableData9 = [];
                   var userTableData10 = [];var userTableData11 = [];var userTableData12 = [];var userTableData13 = [];var userTableData14 = [];
+                  var userTableData15 = [];
                   var data0 = [];var data1 = [];var data2 = [];var data3 = [];var data4 = [];var data5 = [];var data6 = [];var data7 = [];
                   var data8 = [];var data9 = [];var data10 = [];var data11 = [];var data12 = [];var data13 = [];var data14 = [];var data15 = [];
                   var tiele = [];
@@ -69,9 +72,9 @@ var getMonth = (index, res) => {
                   sheetName = itemData.name
                   for (var index in itemData.data) {
                   // 0为表头数据
-                  tempArr = itemData.data[0]
-                  title = itemData.data[0]
-                    if (index === 0) {
+                  tempArr = itemData.data[2]
+                  title = itemData.data[2]
+                    if (index === 0 || index ===1) {
                       continue
                     }
                     var regx = /九江/g;
@@ -79,56 +82,59 @@ var getMonth = (index, res) => {
                     var str2 = itemData.data[index][3] + ''
                     var phoneNums = str.match(regx)
                     if (regx.test(str)) {
-                      // for(let i = 0 ; i < areaArr.length; i++){
-                      //   let rule = new RegExp(areaArr[i],'g')
-                      //   if(str2.match(rule) && i==0){
-                      //     userTableData.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==0){
-                      //     userTableData0.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==1){
-                      //     userTableData1.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==2){
-                      //     userTableData2.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==3){
-                      //     userTableData3.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==4){
-                      //     userTableData4.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==5){
-                      //     userTableData5.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==6){
-                      //     userTableData6.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==7){
-                      //     userTableData7.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==8){
-                      //     userTableData8.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==9){
-                      //     userTableData9.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==10){
-                      //     userTableData10.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==11){
-                      //     userTableData11.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==12){
-                      //     userTableData12.push(str2)
-                      //   }
-                      //   else if(str2.match(rule) && i==12){
-                      //     userTableData13.push(str2)
-                      //   }
-                      // }
+                      for(let i = 0 ; i < areaArr.length; i++){
+                        let rule = new RegExp(areaArr[i],'g')
+                        if(str2.match(rule) && i==0){
+                          userTableData0.push(itemData.data[index])
+                        }
+                        else if(str2.match(rule) && i==1){
+                          userTableData1.push(itemData.data[index])
+                        }
+                        else if(str2.match(rule) && i==2){
+                          userTableData2.push(itemData.data[index])
+                        }
+                        else if(str2.match(rule) && i==3){
+                          userTableData3.push(itemData.data[index])
+                        }
+                        else if(str2.match(rule) && i==4){
+                          userTableData4.push(itemData.data[index])
+                        }
+                        else if(str2.match(rule) && i==5){
+                          userTableData5.push(itemData.data[index])
+                        }
+                        else if(str2.match(rule) && i==6){
+                          userTableData6.push(itemData.data[index])
+                        }
+                        else if(str2.match(rule) && i==7){
+                          userTableData7.push(itemData.data[index])
+                        }
+                        else if(str2.match(rule) && i==8){
+                          userTableData8.push(itemData.data[index])
+                        }
+                        else if(str2.match(rule) && i==9){
+                          userTableData9.push(itemData.data[index])
+                        }
+                        else if(str2.match(rule) && i==10){
+                          userTableData10.push(itemData.data[index])
+                        }
+                        else if(str2.match(rule) && i==11){
+                          userTableData11.push(itemData.data[index])
+                        }
+                        else if(str2.match(rule) && i==12){
+                          userTableData12.push(itemData.data[index])
+                        }
+                        else if(str2 == "位置仅到地市" && i==12){   //i的值只为了循环得到一次userTableData13
+                          userTableData13.push(itemData.data[index])
+                        }
+                        // else if(str2.match(rule) && i==14){
+                        //   userTableData14.push(itemData.data[index])
+                        // }
+                        // else if(str2.match(rule) && i==15){
+                        //   userTableData15.push(itemData.data[index])
+                        // }
+                      }
 
-                      userTableData.push(itemData.data[index])
+                      // userTableData.push(itemData.data[index])
                     }
                   }
                   console.log('走访表数据提取：', ['userTableData'])
@@ -149,16 +155,171 @@ var getMonth = (index, res) => {
                   //由于各列数据长度不同，可以设置一下列宽
                   // const options = {'!cols': [{ wch: 10 }, { wch: 5 }, { wch: 15 }, { wch: 20 } ]};
                   //生成表格
-                  data0.push(title)
-                  data0.push(...userTableData)
-                  // console.log(...userTableData);
-                  var buffer = xlsx.build([{name:'sheet1',data:data0 }]);
-                  // `sheet九江数据：+${userTableData.length}`
-                  var excelNameRegx = /[\u4e00-\u9fa5]+.\b/
-                  var filePath = excelName.match(excelNameRegx) + sheetName + '.xlsx';
-                  var finalPath = path.resolve(__dirname,filePath)
-                  fs.writeFileSync(finalPath,buffer,{'flag':'w'});
-
+                  if(userTableData0.length > 0){
+                    data0.push(title)
+                    data0.push(...userTableData0)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data0 }]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[0] + userTableData0.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData1.length > 0){
+                    data1.push(title)
+                    data1.push(...userTableData1)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data1}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[1] + userTableData1.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData2.length > 0){
+                    data2.push(title)
+                    data2.push(...userTableData2)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data2}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[2] + userTableData2.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData3.length > 0){
+                    data3.push(title)
+                    data3.push(...userTableData3)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data3}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[3] + userTableData3.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData4.length > 0){
+                    data4.push(title)
+                    data4.push(...userTableData4)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data4}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[4] + userTableData4.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData5.length > 0){
+                    data5.push(title)
+                    data5.push(...userTableData5)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data5}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[5] + userTableData5.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData6.length > 0){
+                    data6.push(title)
+                    data6.push(...userTableData6)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data6}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[6] + userTableData6.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData7.length > 0){
+                    data7.push(title)
+                    data7.push(...userTableData7)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data7}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[7] + userTableData7.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData8.length > 0){
+                    data8.push(title)
+                    data8.push(...userTableData8)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data8}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[8] + userTableData8.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData9.length > 0){
+                    data9.push(title)
+                    data9.push(...userTableData9)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data9}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[9] + userTableData9.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData10.length > 0){
+                    data10.push(title)
+                    data10.push(...userTableData10)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data10}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[10] + userTableData10.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData11.length > 0){
+                    data11.push(title)
+                    data11.push(...userTableData11)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data11}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[11] + userTableData11.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData12.length > 0){
+                    data12.push(title)
+                    data12.push(...userTableData12)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data12}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + areaArr[12] + userTableData12.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData13.length > 0){
+                    data13.push(title)
+                    data13.push(...userTableData13)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data13}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + 'others' + userTableData13.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
+                  if(userTableData14.length > 0){
+                    data14.push(title)
+                    data14.push(...userTableData14)
+                    // console.log(...userTableData);
+                    let buffer = xlsx.build([{name:'sheet1',data:data14}]);
+                    // `sheet九江数据：+${userTableData.length}`
+                    let excelNameRegx = /[\u4e00-\u9fa5]+.\b/
+                    let filePath = excelName.match(excelNameRegx) + sheetName + 'areaArr[14]' + userTableData14.length +"条数据" + '.xlsx';
+                    let finalPath = path.resolve(__dirname,filePath)
+                    fs.writeFileSync(finalPath,buffer,{'flag':'w'});
+                  }
                   // 将所有数据写入nodeExcel中
                   // console.log(conf);
                   // const result = nodeExcel.execute([conf])
